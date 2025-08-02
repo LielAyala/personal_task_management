531a84733f956f3e7b283d795c68d10fc14af15a
איילה כהן <131193595+LielAyala@users.noreply.github.com>
2025-08-02 23:02:35 +0300

    הוספת יצוא של ראוטר users_R וחיבור לנתיב גם לlogin

--- index.js
+++ index.js
@@ -20,9 +20,33 @@ global.addSlashes    = require('slashes').addSlashes;
 global.stripSlashes  = require('slashes').stripSlashes;
 
 global.was_logged = false;
+
+
+//יבוא הראוטרים
+const userRouter = require('./Routers/users_R');
+
+//חיבור לנתיב
+app.use("/U", userRouter);
+
+
+
+
+app.post('/login', [userMid.CheckLogin], (req, res) => {
+    if (req.validUser) {
+        res.redirect(""); // ליצור את עמוד המשתמש להוספת משימות ולהדביק כאן
+    } else {
+        res.send("שם משתמש או סיסמה שגויים");
+    }
+});
+app.get('/login', (req, res) => {
+    res.render("login");
+});
+
+
+
+
 app.get('/', (req, res) => {
     res.send("הראוטר עובד ");
-    // res.render("index", {});
 })
 
 app.listen(port, () => {
