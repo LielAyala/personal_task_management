4c40736573a1507308aadf1477ae8224c28a633b
איילה כהן <131193595+LielAyala@users.noreply.github.com>
2025-08-02 23:03:00 +0300

    עדכון

--- Routers/users_R.js
+++ Routers/users_R.js
@@ -10,7 +10,7 @@ router.get("/Add",(req,res)=>{
     });
 });
 router.post("/Add", [user_Mid.AddUser], (req, res) => {
-    res.redirect("/U/List");
+    res.redirect("/login");
 });
 router.get("/Edit/:id",[user_Mid.GetOneUser],(req,res)=>{
     if(req.GoodOne) {
@@ -18,20 +18,10 @@ router.get("/Edit/:id",[user_Mid.GetOneUser],(req,res)=>{
             data: req.one_user_data,
         });
     } else{
-        res.redirect("/Crs/List");
+        res.redirect("/login");
     }
 });
 router.post("/Edit/:id", [user_Mid.UpdateUser], (req, res) => {
-    res.redirect("/U/List");
+    res.redirect("/login");
 });
-router.get("/List",[user_Mid.GetAllUsers],(req,res)=>{
-    res.render("user_list",{
-        page_title  : "רשימת משתמשים",
-        users       : req.users_data,
-        page        : req.page,
-        total_pages : req.total_pages,
-    });
-});
-router.post("/Delete",[user_Mid.DeleteUser],(req,res)=>{
-    res.redirect("/U/List");
-})
+
